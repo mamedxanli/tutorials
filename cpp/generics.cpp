219fc41cac63c3bcc9f72f154e188606e75ff937
Alek Mammadov <mammadov.alek@linux.com>
2020-08-26 16:12:30 +0200

    adding some c++ stuff

---
+++ cpp/generics.cpp
@@ -0,0 +1,9 @@
+//bit fields specify how many bits are allocated to the particular object:
+
+struct preferences {
+bool likesMusic  : 1  //only 1 bit
+unsigned int numberOfChildren : 4  //4 bits
+}
+
+****************************************************************************************
+
